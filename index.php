31c53b5dfb5b4133545beb01db8938e4e09a8eb1
Slava Volokitin <Bindo2007@ukr.net>
2015-09-10 00:51:18 +0300

    index13

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
 
-echo "test g 4585528";
+echo "After a fun trip to Miami, I'm back in Destin and ready for my 22nd Bootcamp to start in less than a week. It's amazing to me how time flies - I so clearly remember my first Bootcamp class and it doesn't seem like it was that long ago! But when I look at those early videos and slides, I'm pleased to see how much the course has improved: we've made a lot of great additions to the content, trimmed the fat, and made an excellent product overall. ";
 
 ?>
